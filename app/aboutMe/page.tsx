5ebc681383cbb7ec10c9293c4eab37e5066955fe
salee <kapitailismho@gmail.com>
2023-05-06 03:43:29 +0900

    20230506

---
+++ app/aboutMe/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function aboutMePage() {
+  return (
+    <h1>
+      <Link href="/">return to home</Link>
+    </h1>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <h1>
+      <Link href="/app">going to aboutMe</Link>
+    </h1>
+  );
+}
