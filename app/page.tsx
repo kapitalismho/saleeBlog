76651bbe26ca7f2f69e361914f599046fd800d7f
salee <kapitailismho@gmail.com>
2023-05-18 08:00:51 +0900

    home development complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 
 import styles from "./page.module.css";
-
 import Link from "next/link";
 import Image from "next/image";
-
 import homepic from "./avatar.png";
+import {
+  DiscordIcon,
+  GmailIcon,
+  GithubIcon,
+  LinkedinIcon,
+} from "./components/icons";
 
 export default function HomePage() {
   return (
@@ -18,6 +22,36 @@ export default function HomePage() {
               alt="ilust of the salee avatar, click to move aboutme page"
               />
         </Link>
+        <aside className={styles.iconContainer}>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.discord.com/users/377814093182140416"
+          >
+            <DiscordIcon />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:kapitalismho@gmail.com"
+          >
+            <GmailIcon />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/kapitalismho"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/sabin-lee-b3b190218/"
+          >
+            <LinkedinIcon />
+          </a>
+        </aside>
       </div>
     </main>
   );
