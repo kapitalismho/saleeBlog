99d1aa5fbace683e1dbd4c5dc5f3161a21015fed
salee <kapitailismho@gmail.com>
2023-05-17 11:56:56 +0900

    homepage page (in development)

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,21 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-import homepic from "./avatar.jpg";
+import homepic from "./avatar.png";
 
 export default function HomePage() {
   return (
-    <div>
-      <Link href="/aboutMe">
-        <div>
-          <h1>
-            <p>SALEE</p>
-          </h1>
-        </div>
-        <div>
-          <Image
-            src = {homepic}
-            alt = "Picture of the salee avatar"
-            />
-        </div>
-      </Link>
-    </div>
+    <main className={styles.topContainer}>
+      <div className={styles.linkContainer}>
+        <Link href="/aboutMe">
+            <Image className={styles.imageContainer}
+              src={homepic}
+              width={1320}
+              height={1614}
+              alt="ilust of the salee avatar, click to move aboutme page"
+              />
+        </Link>
+      </div>
+    </main>
   );
 }
